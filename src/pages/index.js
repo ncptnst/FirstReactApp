0b1441b16f853abdac91bb1d7f8eff193d97558f
Seyit Yilmaz <seyityilmaz@FPGA-Storage01.redmond.corp.microsoft.com>
2018-08-14 21:05:27 -0700

    Commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hey,</h1>
+    <p>Help youselves.</p>
+    
+
+    <Link to="/page-2/">Go to page 2 now</Link>
+  </div>
+)
+
+export default IndexPage
